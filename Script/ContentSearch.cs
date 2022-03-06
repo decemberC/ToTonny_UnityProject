7c8becdafa7a6ce2c0c3bd7120f3165e62f32323
DecemberC <wanghoc71@gmail.com>
2022-03-07 02:10:03 +0800

    From December: UPDATED: -Now the Lnq can get the items. Last time you test scan "OREO" failed due to the case problem -Test ContentSearch is use for the testing. You are no need to scan the items if you use it.

--- Script/ContentSearch.cs
+++ Script/ContentSearch.cs
@@ -6,15 +6,41 @@ using UnityEngine.UI;
 
 public class ContentSearch : MonoBehaviour
 {
-    
-    public static DatabaseClass SearchItem(string feedback) {
+
+    public static DatabaseClass SearchItem(string feedback)
+    {
         Debug.Log("test content");
-        /*var result = from i in DataManager.DB
-                        group i by i.KeyWord into x
-                        where feedback.Contains(x.Key)
-                        select new { x.Key};*/
-        Debug.Log(feedback.Contains("OREO"));
+        var result = from i in DataManager.DB
+                     group new
+                     {
+                         i.Allergy,
+                         i.Carbohydrate,
+                         i.Energy
+                     ,
+                         i.Gluten,
+                         i.Name,
+                         i.Protein,
+                         i.Saturated,
+                         i.Shellfish
+                     ,
+                         i.Sodium,
+                         i.Sugars,
+                         i.TransFat
+                     } by i.KeyWord into x
+                     where feedback.Contains(x.Key)
+                     select x;
+        Debug.Log(result.Count());
+        foreach (var searchRes in result)
+        {Debug.Log(searchRes.Count());
+            foreach (var data in searchRes)
+            {
+                if (feedback.Contains(data.Name))
+                {
+                    Debug.Log(data.Name);
+                }
+            }
+        }
         return null;
 
-    } 
+    }
 }
