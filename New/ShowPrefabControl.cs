8c754f6faf4cf6baf0104a4427def449df4e87b9
DecemberC <wanghoc71@gmail.com>
2022-04-26 20:04:52 +0800

    Fix generate location wrong

--- New/ShowPrefabControl.cs
+++ New/ShowPrefabControl.cs
@@ -15,35 +15,42 @@ public class ShowPrefabControl : MonoBehaviour
         showingprefab ="";
     }
     // Start is called before the first frame update
-    public void ShowPrefabByData(DatabaseClass data, ARTrackedImage item)
+    public void ShowPrefabByData(DatabaseClass data, ARTrackedImage item , Camera posCamera )
     {
         if(showingprefab == ""||item.referenceImage.name ==showingprefab){
-            showingprefab =item.referenceImage.name; 
+            showingprefab =item.referenceImage.name;
+            int showedNum = 0; 
+            //item.transform.SetParent(posCamera.transform.parent);
+           
         foreach (GameObject i in prefabList)
         {
             string pName = item.referenceImage.name + "_" + i.name;
             if (i.name == "Gluten" && data.gluten)
             {
-
+                
                 if (!showList.ContainsKey(pName))
                 {
-                    GameObject instaGameobj = GameObject.Instantiate(i, new Vector3(0 + showList.Count * 0.02f, 0, 0), Quaternion.identity);
-                    instaGameobj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                    GameObject instaGameobj = GameObject.Instantiate(i, new Vector3(0+showedNum*0.02f , 0,0), Quaternion.identity);
+                    showedNum++;
+                    // instaGameobj.transform.localScale =new Vector3(0.02f,0.02f,0.02f); 
                     showList.Add(pName, instaGameobj);
                 }
                 else if (!(showList[pName] == null) && !showList[pName].activeInHierarchy)
                 {
-
-                    showList[pName].transform.position = new Vector3(item.transform.position.x + prefabList.IndexOf(i) * 0.02f, item.transform.position.y, item.transform.position.z);
-                    showList[pName].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                    showList[pName].transform.position = new Vector3(item.transform.position.x+showedNum*0.02f, item.transform.position.y, item.transform.position.z);
+                    //showList[pName].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+                    showedNum++;
                     showList[pName].SetActive(true);
                 }
                 else
                 {
-                    showList[pName].transform.position = new Vector3(item.transform.position.x + prefabList.IndexOf(i) * 0.02f, item.transform.position.y, item.transform.position.z);
                     showList[pName].transform.rotation = item.transform.rotation;
-                    showList[pName].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                    showList[pName].transform.position = new Vector3(item.transform.position.x+showedNum*0.02f, item.transform.position.y, item.transform.position.z);
+                    
+                    showedNum++;
+                    //showList[pName].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
                 }
+                
                 continue;
             }
             if (i.name == "Shellfish" && data.shellfish)
@@ -52,23 +59,30 @@ public class ShowPrefabControl : MonoBehaviour
 
                 if (!showList.ContainsKey(pName))
                 {
-                    GameObject instaGameobj = GameObject.Instantiate(i, new Vector3(0 + showList.Count * 0.02f, 0, 0), Quaternion.identity);
-                    instaGameobj.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+                    GameObject instaGameobj = GameObject.Instantiate(i, new Vector3(0+showedNum*0.02f , 0,0), Quaternion.identity,item.transform);
+                    showedNum++;
+                    //instaGameobj.transform.localScale =new Vector3(0.02f,0.02f,0.02f); 
+                    Debug.Log(instaGameobj.transform.lossyScale);
                     showList.Add(pName, instaGameobj);
                 }
                 else if (!(showList[pName] == null) && !showList[pName].activeInHierarchy)
                 {
-
-                    showList[pName].transform.position = new Vector3(item.transform.position.x + prefabList.IndexOf(i) * 0.02f, item.transform.position.y, item.transform.position.z);
-                    showList[pName].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                    showList[pName].transform.position = new Vector3(item.transform.position.x+showedNum*0.02f, item.transform.position.y, item.transform.position.z);
+                    //showList[pName].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+                    
+                    showedNum++;
+                    Debug.Log(showList[pName].transform.position);
                     showList[pName].SetActive(true);
 
                 }
                 else
                 {
-                    showList[pName].transform.position = new Vector3(item.transform.position.x + prefabList.IndexOf(i) * 0.02f, item.transform.position.y, item.transform.position.z);
                     showList[pName].transform.rotation = item.transform.rotation;
-                    showList[pName].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                    showList[pName].transform.position = new Vector3(item.transform.position.x+showedNum*0.02f, item.transform.position.y, item.transform.position.z);
+                    
+                    //showList[pName].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+                    showedNum++;
+                    Debug.Log(showList[pName].transform.position);
                 }
                 continue;
             }
