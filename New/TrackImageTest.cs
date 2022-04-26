a6faf37a71fe1d1f9b3a8c0579986fbde111261d
DecemberC <wanghoc71@gmail.com>
2022-04-26 20:20:46 +0800

    scipt update

---
+++ New/TrackImageTest.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+using UnityEngine.TestTools;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.Rendering;
+public class TrackImageTest : MonoBehaviour
+{
+    public Camera arCam;
+
+    private DatabaseClass searchResult;
+    public Image image;
+    public ARTrackedImageManager imanager;
+    public Light ilight;
+    public Text result;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        imanager.trackedImagePrefab = new GameObject("Emptypref");
+        imanager.trackedImagesChanged += ImageReturn;
+
+    }
+    void OnDisable()
+    {
+        imanager.trackedImagesChanged -= ImageReturn;
+
+    }
+    public void ImageReturn(ARTrackedImagesChangedEventArgs args)
+    {
+        arCam.transform.rotation =Quaternion.identity;
+       
+        foreach (var item in args.updated)
+        {
+            if (item.trackingState == TrackingState.Tracking)
+            {
+                string serachName = item.referenceImage.name.Remove(item.referenceImage.name.IndexOf('_'));
+                //WebCamTextureToCloudVision.aiSearch.ActivateCapture(arCam, item.referenceImage.name);
+               
+                    searchResult = ContentSearch.SearchItem(serachName);
+                    //result.text = item.transform.parent.transform.localScale.ToString();
+                    
+                    ShowPrefabControl.self.ShowPrefabByData(searchResult, item, arCam);
+                    
+                /*if (searchResult != null)
+                {
+                    ShowPrefabControl.self.ShowPrefabByData(searchResult, item);
+                }*/
+
+                //result.text = item.referenceImage.name;
+
+            }
+        }
+        if (args.updated.Count != 0)
+        {
+            foreach (var delItem in imanager.trackables)
+            {
+                if (delItem.trackingState != TrackingState.Tracking)
+                {
+                    string serachName = delItem.referenceImage.name.Remove(delItem.referenceImage.name.IndexOf('_'));
+                    if (args.added.Contains(delItem))
+                        args.added.Remove(delItem);
+                    if (args.updated.Contains(delItem))
+                        args.updated.Remove(delItem);
+                    WebCamTextureToCloudVision.aiSearch.ReleaseCapture(delItem.referenceImage.name);
+                    ShowPrefabControl.self.NoShowPrefab(delItem);
+                    
+                    
+                }
+            }
+        }
+        //result.text = args.updated.Count.ToString();
+    }
+
+}
