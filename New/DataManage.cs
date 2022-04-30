38aaada685c9433d4c2e5feeaef21f59c776d7b0
DecemberC <wanghoc71@gmail.com>
2022-04-30 22:14:38 +0800

    push the one forget Push

--- New/DataManage.cs
+++ New/DataManage.cs
@@ -2,34 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManage:MonoBehaviour
+public class DataManage : MonoBehaviour
 {
     public static string colorPicker;
     public static DataManage self;
-    public static Dictionary<string,string> languageTable = new Dictionary<string, string>();
-    public static string appLan ;
+    public static Dictionary<string, string> languageTable = new Dictionary<string, string>();
+    public static string appLan;
     public List<DatabaseClass> DB;
     void Start()
     {
-        self=this;
-        if(languageTable.Count == 0){
-            
-            appLan="En";
+        self = this;
+        if (languageTable.Count == 0)
+        {
+            appLan = "En";
             SetLanguageTable(appLan);
         }
     }
-    public static void SetLanguageTable(string language){
-        if(languageTable.Count!=0){
+    public static void SetLanguageTable(string language)
+    {
+        if (languageTable.Count != 0)
+        {
             languageTable.Clear();
         }
-        var loader = Resources.Load<TextAsset>("Language/"+language);
-        
-        string[] matchLanguage = loader.text.Split(new string[] {";\r\n"},System.StringSplitOptions.None);
-        
-     
-        foreach(string i in matchLanguage){
+        var loader = Resources.Load<TextAsset>("Language/" + language);
+
+        string[] matchLanguage = loader.text.Split(new string[] { ";\r\n" }, System.StringSplitOptions.None);
+
+
+        foreach (string i in matchLanguage)
+        {
             string[] ceil = i.Split(':');
-            languageTable.Add(ceil[0],ceil[1]);
-        }        
+            string z = "";
+            for (int x = 1; x < ceil.Length; x++)
+            {
+
+                z += ceil[x];
+                if (x != ceil.Length){
+                    z+=":";
+                }
+            }
+            languageTable.Add(ceil[0], ceil[1]);
+        }
     }
 }
